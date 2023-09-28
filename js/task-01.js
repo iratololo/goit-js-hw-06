7eb3801ab31f0063edac9dd6bcfffbad1a80533e
iratololo <iratololo1991@gmail.com>
2023-09-28 21:08:16 +0300

    task-02

---
+++ js/task-01.js
@@ -0,0 +1,5 @@
+const categories = document.querySelector("#categories");
+console.log('Number of categories:', categories.children.length);
+// console.log(Array.isArray(categories));
+const items = document.querySelectorAll(".item");
+console.log(items);
--- js/task-02.js
+++ js/task-02.js
@@ -6,3 +6,14 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+const list = document.querySelector("#ingredients");
+
+const murkup = ingredients.map((elem) => {
+  const li = document.createElement("li");
+  li.textContent = elem;
+  li.classList.add("item");
+  return li;
+});
+
+list.append(...murkup);
